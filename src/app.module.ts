65e8934ce20b1ab5b31aee42f01a7c38f8c1019d
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-04-09 05:14:50 +0700

    feat: node mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +9,8 @@ import { APP_FILTER } from '@nestjs/core';
 import { ExceptionLoggerFilter } from './utils/exceptionLogger.filter';
 import { MediaModule } from './media/media.module';
 import { SubscriberModule } from './subscriber/subscriber.module';
+import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -21,6 +23,31 @@ import { SubscriberModule } from './subscriber/subscriber.module';
     UserModule,
     MediaModule,
     SubscriberModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        // transport: config.get('MAIL_TRANSPORT'),
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.get('MAIL_FROM')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'src/templates/email'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [
