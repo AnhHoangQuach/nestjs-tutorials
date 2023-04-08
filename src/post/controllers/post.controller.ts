be4068aac6c8c9ba2685a4a9d823dde9dde7d800
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-04-09 04:55:49 +0700

    feat: cqrs and cache

--- src/post/controllers/post.controller.ts
+++ src/post/controllers/post.controller.ts
@@ -10,6 +10,10 @@ import {
   UseGuards,
   Req,
   Query,
+  CACHE_MANAGER,
+  CacheInterceptor,
+  Inject,
+  UseInterceptors,
 } from '@nestjs/common';
 import {
   CreatePostDto,
@@ -20,10 +24,37 @@ import { PostService } from '../services/post.service';
 import { ExceptionLoggerFilter } from '../../utils/exceptionLogger.filter';
 import { HttpExceptionFilter } from '../../utils/httpException.filter';
 import { AuthGuard } from '@nestjs/passport';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CreatePostCommand } from '../commands/createPost.command';
+import { GetPostQuery } from '../queries/getPost.query';
+import { Cache } from 'cache-manager';
 
 @Controller('post')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  @Get(':id/get-with-cache')
+  @UseInterceptors(CacheInterceptor)
+  async getPostDetailWithCache(@Param('id') id: string) {
+    console.log('Run here');
+    return (await this.postService.getPostById(id)).toJSON();
+  }
+
+  @Get('cache/demo/set-cache')
+  async demoSetCache() {
+    await this.cacheManager.set('newnet', 'hello world', { ttl: 60 * 10 });
+    return true;
+  }
+
+  @Get('cache/demo/get-cache')
+  async demoGetCache() {
+    return this.cacheManager.get('newnet');
+  }
 
   @Get()
   getAllPost(@Query() { page, limit, start }: PaginationPostDto) {
@@ -35,6 +66,17 @@ export class PostController {
   // @UseFilters(ExceptionLoggerFilter)
   getPostById(@Param('id') id: string) {
     return this.postService.getPostById(id);
+  }
+
+  @Get(':id/get-by-query')
+  async getDetailByQuery(@Param('id') id: string) {
+    return this.queryBus.execute(new GetPostQuery(id));
+  }
+
+  @Post('create-by-command')
+  @UseGuards(AuthGuard('jwt'))
+  async createByCommand(@Req() req: any, @Body() post: CreatePostDto) {
+    return this.commandBus.execute(new CreatePostCommand(req.user, post));
   }
 
   @Post()
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostController } from './controllers/post.controller';
 import { PostSchema } from './models/post.model';
@@ -9,6 +9,11 @@ import { CategorySchema } from './models/category.model';
 import { CategoryController } from './controllers/category.controller';
 import { CategoryRepository } from './repositories/category.repository';
 import { CategoryService } from './services/category.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CreatePostHandler } from './handler/createPost.handler';
+import { GetPostHandler } from './handler/getPost.handler';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -22,9 +27,29 @@ import { CategoryService } from './services/category.service';
         schema: CategorySchema,
       },
     ]),
+    CqrsModule,
     UserModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        // isGlobal: true,
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        username: configService.get<string>('REDIS_USERNAME'),
+        password: configService.get<string>('REDIS_PASSWORD'),
+      }),
+    }),
   ],
   controllers: [PostController, CategoryController],
-  providers: [PostService, PostRepository, CategoryRepository, CategoryService],
+  providers: [
+    PostService,
+    PostRepository,
+    CategoryRepository,
+    CategoryService,
+    CreatePostHandler,
+    GetPostHandler,
+  ],
 })
 export class PostModule {}
